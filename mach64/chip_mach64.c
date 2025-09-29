1f9ec90d3a5934612e071c5952f8fde61f0b0069
mheyer <sonode@gmx.de>
2025-09-28 23:51:32 -0700

    Fix DAC initialization

--- mach64/chip_mach64.c
+++ mach64/chip_mach64.c
@@ -403,10 +403,12 @@ void InitVClockPLLTable(BoardInfo_t *bi, const BYTE *multipliers, BYTE numMultip
 #define DAC_MASK    2
 #define DAC_R_INDEX 3
 
-#define DAC_BLANKING      BIT(0)
-#define DAC_BLANKING_MASK BIT(0)
-#define DAC_8BIT_EN       BIT(8)
-#define DAC_8BIT_EN_MASK  BIT(8)
+#define DAC_BLANKING        BIT(2)
+#define DAC_BLANKING_MASK   BIT(2)
+#define DAC_8BIT_EN         BIT(8)
+#define DAC_8BIT_EN_MASK    BIT(8)
+#define DAC_VGA_ADR_EN      BIT(13)
+#define DAC_VGA_ADR_EN_MASK BIT(13)
 
 static UWORD CalculateBytesPerRow(__REGA0(struct BoardInfo *bi), __REGD0(UWORD width), __REGD1(UWORD height),
                                   __REGA1(struct ModeInfo *mi), __REGD7(RGBFTYPE format))
@@ -2259,8 +2261,8 @@ BOOL InitChip(__REGA0(struct BoardInfo *bi))
         bi->MemorySize -= 2048;  // Upper 2kb are reserved for MMIO register blocks 0 and 1
     }
 
-    // Init DAC
-    W_BLKIO_MASK_L(DAC_CNTL, DAC_8BIT_EN_MASK | DAC_BLANKING_MASK, DAC_8BIT_EN | DAC_BLANKING);
+    // Init DAC. Enable 8bit per gun, blanking pedestal and disable decoding DAC at legacy IO address
+    W_BLKIO_MASK_L(DAC_CNTL, DAC_8BIT_EN_MASK | DAC_BLANKING_MASK | DAC_VGA_ADR_EN, DAC_8BIT_EN | DAC_BLANKING);
     W_BLKIO_B(DAC_REGS, DAC_MASK, 0xFF);
 
     // Init CRTC, Display FIFO Low Water Mark
