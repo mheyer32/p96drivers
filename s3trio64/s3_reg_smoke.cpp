74adc71968acbfcd04dcaeef38f81da0811d7fa9
mheyer <sonode@gmx.de>
2026-08-11 12:23:28 -0700

    remove smoke tests

--- s3trio64/s3_reg_smoke.cpp
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-#include "s3_reg_apertures.hpp"
-#include "s3config.h"
-ULONG s3CxxRegSmokeReadGpStat(volatile UBYTE *ioBase)
-{
-    return S3IoQ(ioBase).readW(static_cast<IoReg::Id>(0x9AE8));
-}
